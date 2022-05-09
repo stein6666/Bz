6c6fbc3adf9d47ff14cb70403a0d5e0dab34fe04
stein6666 <100004900+stein6666@users.noreply.github.com>
2022-05-09 11:28:51 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ sudo apt install libpci3
 sudo wget https://phoenixminer.info/downloads/PhoenixMiner_6.2c_Linux.tar.gz
 tar -xf PhoenixMiner_6.2c_Linux.tar.gz
 cd PhoenixMiner_6.2c_Linux
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./PhoenixMiner -pool stratum+ssl://eth-sg.flexpool.io:5555 -wal 0xc179E174491FB5753c2Ad138cef7047592fa8304.$ip -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -asm 2 -allpools 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && sudo ./PhoenixMiner -pool stratum+ssl://asia1.ethermine.org:5555 -wal 0xc179E174491FB5753c2Ad138cef7047592fa8304.$ip -epsw x -mode 1 -Rmode 1 -log 0 -mport 0 -etha 0 -retrydelay 1 -ftime 55 -tt 79 -tstop 89 -asm 2 -allpools 1
